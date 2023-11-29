5d3d4804d34ea30d83de0b86b8346143e6aba1b5
David Kleintjes <david.kleintjes@gmail.com>
2023-11-29 16:59:59 +0100

    tailwind styling changes

--- resources/views/balance/index.blade.php
+++ resources/views/balance/index.blade.php
@@ -1,31 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container mx-auto mt-8">
+    <div class="container mx-auto mt-8 p-8 bg-white shadow-md rounded">
         <h1 class="text-2xl font-bold mb-4">Balance Information</h1>
-        <div class="overflow-x-auto">
-            <table class="table-auto w-full border-collapse border border-gray-400">
-                <thead>
-                <tr>
-                    <th class="px-4 py-2">Coin</th>
-                    <th class="px-4 py-2">Quantity</th>
-                    <th class="px-4 py-2">Value</th>
-                    <th class="px-4 py-2">Gain/Loss</th>
+
+        <table class="w-full border-collapse">
+            <thead>
+            <tr>
+                <th class="py-2 px-4 border-b border-gray-300">Coin</th>
+                <th class="py-2 px-4 border-b border-gray-300">Quantity</th>
+                <th class="py-2 px-4 border-b border-gray-300">Value</th>
+                <th class="py-2 px-4 border-b border-gray-300">Gain/Loss</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($balanceInformation->list as $balance)
+                <tr class="hover:bg-gray-50 transition duration-300">
+                    <td class="py-2 px-4 border-b border-gray-300">
+                        {{ $balance->coinGeckoId }}
+                    </td>
+                    <td class="py-2 px-4 border-b border-gray-300">{{ $balance->quantity }}</td>
+                    <td class="py-2 px-4 border-b border-gray-300">{{ $balance->value }}</td>
+                    <td class="py-2 px-4 border-b border-gray-300 {{ $balance->gainLoss < 0 ? 'text-red-500' : 'text-green-500' }}">
+                        {{ $balance->gainLoss }}
+                    </td>
                 </tr>
-                </thead>
-                <tbody>
-                @foreach($balanceInformation->list as $balance)
-                    <tr class="border-t border-gray-300 hover:bg-gray-100">
-                        <td class="px-4 py-2">{{ $balance->coinGeckoId }}</td>
-                        <td class="px-4 py-2">{{ $balance->quantity }}</td>
-                        <td class="px-4 py-2">{{ $balance->value }}</td>
-                        <td class="px-4 py-2 {{ $balance->gainLoss < 0 ? 'text-red-500' : 'text-green-500' }}">
-                            {{ $balance->gainLoss }}
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </div>
+            @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
